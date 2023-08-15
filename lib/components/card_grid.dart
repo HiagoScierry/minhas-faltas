a078485623437dedf8e2a71f7f45fc1af2c76b9e
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-08-15 17:21:18 -0300

    terminado estilização da modals

--- lib/components/card_grid.dart
+++ lib/components/card_grid.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'custom_card.dart';
 
 class CardItem {
   final String title;
-  final int percentage;
+  final int hours;
+  final int faults = 0;
 
-  CardItem({required this.title, required this.percentage});
+  CardItem({required this.title, required this.hours});
 }
 
 class CardGrid extends StatelessWidget {
@@ -27,23 +28,14 @@ class CardGrid extends StatelessWidget {
       itemBuilder: (context, index) {
         return CustomCard(
           title: cardItems[index].title,
-          percentage: cardItems[index].percentage,
+          percentage: cardItems[index].hours,
           tapHandle: () {
             // Show the AlertDialog with TextField
             showDialog(
               context: context,
               builder: (context) {
-                return AlertDialog(
-                  title: Text('Modal ${index + 1}'),
-                  content: CustomModalContent(title: cardItems[index].title),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Close'),
-                    ),
-                  ],
+                return CustomModalContent(
+                  cardItem: cardItems[index],
                 );
               },
             );
@@ -54,42 +46,85 @@ class CardGrid extends StatelessWidget {
   }
 }
 
-class CustomModalContent extends StatelessWidget {
-  final String title;
+class CustomModalContent extends StatefulWidget {
+  final CardItem cardItem;
 
-  CustomModalContent({super.key, required this.title});
+  CustomModalContent({super.key, required this.cardItem});
+
+  @override
+  _CustomModalContentState createState() => _CustomModalContentState();
+}
+
+class _CustomModalContentState extends State<CustomModalContent> {
+  int currentAbsences = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    currentAbsences = widget.cardItem.hours ~/ 10;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+    return AlertDialog(
+      title: Text(widget.cardItem.title),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Faltas Anteriores: ${widget.cardItem.hours ~/ 10}'),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(CircleBorder()),
+                ),
+                onPressed: () {
+                  setState(() {
+                    if (currentAbsences > 0) {
+                      currentAbsences--;
+                    }
+                  });
+                },
+                child: Icon(Icons.remove),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'This is the content of the modal.',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Close the modal
-              },
-              child: Text('Close'),
-            ),
-          ],
-        ),
+              SizedBox(width: 16),
+              Text('$currentAbsences', style: TextStyle(fontSize: 20)),
+              SizedBox(width: 16),
+              ElevatedButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(CircleBorder()),
+                ),
+                onPressed: () {
+                  setState(() {
+                    currentAbsences++;
+                  });
+                },
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
+        ],
       ),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            // Implement actions here, e.g., save the current absences
+            Navigator.pop(context);
+          },
+          child: Text('Salvar'),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+            child: Text('Cancelar'))
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 import 'components/card_grid.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Modal Form Example',
+      title: 'Minhas Materias',
       home: MyHomePage(),
     );
   }
@@ -19,13 +23,24 @@ class MyApp extends StatelessWidget {
 class ListItems {}
 
 class MyHomePage extends StatelessWidget {
-  var content;
+  final List<CardItem> cardItems = [
+    CardItem(title: 'Vetores e Geometria Analítica', hours: 60),
+    CardItem(title: 'Tópicos em Programação 2', hours: 30),
+    CardItem(title: 'Segurança e Auditoria de Sistemas', hours: 60),
+    CardItem(title: 'Interface Humano Computador', hours: 60),
+    CardItem(title: 'Metodologia de Pesquisa', hours: 30),
+    CardItem(title: 'Projetos de Sistemas de Software', hours: 60),
+
+    // ... adicione mais itens aqui
+  ];
+
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Modal Form Example'),
+        title: const Text('Suas Matérias'),
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
@@ -33,18 +48,7 @@ class MyHomePage extends StatelessWidget {
               showDialog(
                 context: context,
                 builder: (context) {
-                  return AlertDialog(
-                    title: const Text('Adicionar Materia'),
-                    content: AddItemModal(),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text('Close'),
-                      ),
-                    ],
-                  );
+                  return const AddFormDialog();
                 },
               );
             },
@@ -52,55 +56,107 @@ class MyHomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: CardGrid(
-          cardItems: [
-            CardItem(title: 'Vetores e Geometria Analítica', percentage: 20),
-            CardItem(title: 'Tópicos em Programação 2', percentage: 30),
-            CardItem(
-                title: 'Segurança e Auditoria de Sistemas', percentage: 40),
-            CardItem(title: 'Interface Humano Computador', percentage: 40),
-            CardItem(title: 'Metodologia de Pesquisa', percentage: 40),
-            CardItem(title: 'Projetos de Sistemas de Software', percentage: 40),
-            // ... adicione mais itens aqui
-          ],
-        ),
+        child: CardGrid(cardItems: cardItems),
       ),
     );
   }
 }
 
-class AddItemModal extends StatelessWidget {
+class AddFormDialog extends StatefulWidget {
+  const AddFormDialog({super.key});
+
+  @override
+  _AddFormDialogState createState() => _AddFormDialogState();
+}
+
+class _AddFormDialogState extends State<AddFormDialog> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  String _selectedHours = '30';
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Perform your form submission here
+      final subjectName = _nameController.text;
+      final subjectHours = _selectedHours;
+
+      // You can process the data or show a success message
+      print('Subject Name: $subjectName');
+      print('Subject Hours: $subjectHours');
+
+      // Close the dialog
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.all(16),
+    return AlertDialog(
+      title: const Text('Adicionar Nova Materia'),
+      content: Form(
+        key: _formKey,
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            const Text(
-              'Add Item',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                labelText: 'Nome da Materia',
+                border: OutlineInputBorder(),
               ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a subject name';
+                }
+                return null;
+              },
             ),
             const SizedBox(height: 16),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Item Name',
+            DropdownButtonFormField<String>(
+              value: _selectedHours,
+              onChanged: (newValue) {
+                setState(() {
+                  _selectedHours = newValue!;
+                });
+              },
+              items: ['30', '60', '90'].map((hours) {
+                return DropdownMenuItem<String>(
+                  value: hours,
+                  child: Text('$hours Horas'),
+                );
+              }).toList(),
+              decoration: const InputDecoration(
+                labelText: 'Carga Horária',
                 border: OutlineInputBorder(),
               ),
             ),
             const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Handle form submission here
-                Navigator.pop(context); // Close the modal
-              },
-              child: const Text('Add'),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: _submitForm,
+                child: const Text('Adicionar'),
+              ),
             ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancelar'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
+              ),
+            )
           ],
         ),
       ),
