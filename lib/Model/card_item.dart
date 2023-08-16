5e661c62bc37fa3fde82e57b3fe817c9e1bb2164
Hiago Moreira <47890178+HiagoScierry@users.noreply.github.com>
2023-08-16 12:25:22 -0300

    atualizado nome da pasta

--- lib/Model/card_item.dart
+++ lib/Model/card_item.dart
@@ -4,4 +4,14 @@ class CardItem {
   final int faults = 0;
 
   CardItem({required this.title, required this.hours});
+
+  addFaults() {
+    faults + 1;
+  }
+
+  removeFaults() {
+    if (faults > 0) {
+      faults - 1;
+    }
+  }
 }
